59fd46126185b7f8c90a2491798feb7590fc0a52
Rossi <syafi.arrosyid@gmail.com>
2023-10-02 12:44:32 +0700

    add api ucup

--- app/Http/Controllers/Api/CPersediaan.php
+++ app/Http/Controllers/Api/CPersediaan.php
@@ -4,83 +4,19 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Persediaan;
+use App\Repositories\ApiHandle\ApiHandleRepository;
 use Illuminate\Http\Request;
 
 class CPersediaan extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
+    protected $apiHandleRepository;
+    function __construct(ApiHandleRepository $apiHandleRepository){
+        $this->apiHandleRepository = $apiHandleRepository;
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Persediaan  $persediaan
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Persediaan $persediaan)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Persediaan  $persediaan
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Persediaan $persediaan)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Persediaan  $persediaan
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Persediaan $persediaan)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Persediaan  $persediaan
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Persediaan $persediaan)
-    {
-        //
+    function list() {
+        return $this->apiHandleRepository->safeApiCall(function(){
+            $persediaan = Persediaan::all();
+            return responseSuccess($persediaan);
+        });
     }
 }
