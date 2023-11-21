911ebdec2f9c9540cf7ff365c192eb19cdf89f17
Dev'o Web-Andro <55377597+Devoandroweb@users.noreply.github.com>
2023-11-21 11:37:46 +0700

    a

--- app/Traits/CreatedBy.php
+++ app/Traits/CreatedBy.php
@@ -6,7 +6,7 @@ trait CreatedBy
 {
     protected static function boot()
     {
-        dd(Auth::guard('cris')->user());
+        dd(Auth::guard('cris')->user().Auth::guard('cris')->check());
         parent::boot();
         static::addGlobalScope('created_by', function ($builder) {
             $builder->where('created_by', Auth::guard('cris')->user()?->id_user);
