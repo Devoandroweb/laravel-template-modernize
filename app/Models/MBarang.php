28cb543da6abf6a3f667e8b42fe031926fb2f77b
Dev'o Web-Andro <55377597+Devoandroweb@users.noreply.github.com>
2023-11-21 11:22:12 +0700

    a

--- app/Models/MBarang.php
+++ app/Models/MBarang.php
@@ -4,14 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Repositories\SystemEpicRepository;
 class MBarang extends Model
 {
     use HasFactory;
     protected $table = 'barang';
     protected $primaryKey = 'id_barang';
     protected $fillable = ['kode_barang','nama_barang', 'satuan', 'id_kategori', 'minimal_persediaan'];
-    // protected $systemEpicRepository;
     
     function kategori(){
         return $this->hasOne(MKategori::class,'id_kategori','id_kategori');
