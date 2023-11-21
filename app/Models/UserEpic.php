41bb1a7d99bf8c2078b6534de423b9007e6d12b7
Dev'o Web-Andro <55377597+Devoandroweb@users.noreply.github.com>
2023-11-21 11:49:13 +0700

    a

--- app/Models/UserEpic.php
+++ app/Models/UserEpic.php
@@ -13,5 +13,5 @@ class UserEpic extends Authenticatable
     use HasApiTokens, HasFactory, Notifiable;
     protected $table = 'user';
     protected $primaryKey = 'id_user';
-    protected $fillable = ['nama', 'email', 'nama_toko','role', 'username', 'password','foto'];
+    protected $fillable = ['nama', 'email', 'nama_toko','role', 'username', 'password','foto','token'];
 }
