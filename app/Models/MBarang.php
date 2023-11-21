ce19334ef4c8bb103145b8ee6df4b023daad0817
Dev'o Web-Andro <55377597+Devoandroweb@users.noreply.github.com>
2023-11-21 13:26:21 +0700

    a

--- app/Models/MBarang.php
+++ app/Models/MBarang.php
@@ -2,9 +2,9 @@
 
 namespace App\Models;
 
+use App\Traits\CreatedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Traits\CreatedBy;
 
 class MBarang extends Model
 {
