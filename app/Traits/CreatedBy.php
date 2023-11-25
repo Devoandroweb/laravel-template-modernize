28b195847bfb22d3e76ecafb18e9ad3b3bed66a5
Dev'o Web-Andro <55377597+Devoandroweb@users.noreply.github.com>
2023-11-25 09:13:53 +0700

    a

--- app/Traits/CreatedBy.php
+++ app/Traits/CreatedBy.php
@@ -8,8 +8,9 @@ trait CreatedBy
 {
     static function whereUser(){
         if(request()->user()->role != 1){
-            return self::where('created_by',request()->user()?->id_user)->get();
+            return self::where('created_by',request()->user()?->id_user);
         }
+        return new static;
     }
     static function whereCreatedBy(){
         return parent::where('created_by',request()->user()?->id_user);
