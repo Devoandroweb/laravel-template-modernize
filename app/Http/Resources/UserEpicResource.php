e312d1ceb92608776e691607241b6acd82c8f260
Dev'o Web-Andro <55377597+Devoandroweb@users.noreply.github.com>
2023-11-25 11:02:39 +0700

    a

--- app/Http/Resources/UserEpicResource.php
+++ app/Http/Resources/UserEpicResource.php
@@ -21,7 +21,12 @@ class UserEpicResource extends JsonResource
             "email" => $this->email,
             "username" => $this->username,
             "role" => $this->role,
-            "foto" => url('public/images/user/'.$this->foto)
+            "foto" => url('public/images/user/'.$this->foto),
+            "alamat" => $this->alamat,
+            "ttl" => $this->ttl,
+            "tempat_lahir" => $this->tempat_lahir,
+            "jk" => $this->jk,
+            "no_tlp" => $this->no_tlp,
         ];
     }
 }
