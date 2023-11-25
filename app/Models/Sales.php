890f88c985d20b15db07f7fd5c59b540b57513c6
Dev'o Web-Andro <55377597+Devoandroweb@users.noreply.github.com>
2023-11-25 09:05:10 +0700

    a

--- app/Models/Sales.php
+++ app/Models/Sales.php
@@ -8,11 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Sales extends Model
 {
-    use HasFactory,CreatedBy;
+    use HasFactory;
     protected $table = 'sales';
     protected $primaryKey = 'id_sales';
     protected $fillable = ['id_barang', 'jumlah_sales', 'tanggal_sales'];
     function barang(){
         return $this->hasOne(MBarang::class,'id_barang','id_barang');
+    }
+    static function whereUser(){
+        if(request()->user()->role != 1){
+            return self::where('created_by',request()->user()?->id_user)->get();
+        }
+        return self;
     }
 }
