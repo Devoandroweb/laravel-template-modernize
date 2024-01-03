3219c47883a387b6730cfba9153d8285c27b0bc0
Dev'o Web-Andro <55377597+Devoandroweb@users.noreply.github.com>
2024-01-03 16:00:03 +0700

    a

--- app/Traits/CreatedBy.php
+++ app/Traits/CreatedBy.php
@@ -8,10 +8,10 @@ trait CreatedBy
 {
     static function whereUser(){
         if(request()->user()->role != 1){ #bukan admin
-            return self::where('created_by',request()->user()?->id_user)->orderBy('desc');
+            return self::where('created_by',request()->user()?->id_user)->orderBy('created_at','desc');
         }else{
             if(request('id_user') != 0){
-                return self::where('created_by',request('id_user'))->orderBy('desc');
+                return self::where('created_by',request('id_user'))->orderBy('created_at','desc');
             }
         }
         return new static;
